d2830c17fe991988811185b36a2c6a1d8e6d0b67
gitjua <juakali422@gmail.com>
2022-06-14 09:06:54 -0600

    from Home state

--- src/Home.js
+++ src/Home.js
@@ -5,15 +5,17 @@ const Home = () => {
 
   const [name, setName] = useState('Mario');
   const [age, setAge] = useState(25);
+  const [from, setFrom] = useState('Boston');
   const handleClick = (e) => {
    setName('luigi');
    setAge(30);
+   setFrom('Salt lake City');
   }
 
   return (
     <div className="home">
       <h2>Homepage</h2>
-      <p>{name} age is {age} years old</p>
+      <p>{name} age is {age} years old and is from {from}</p>
       <button onClick={handleClick}>Click me</button>
       
     </div>
