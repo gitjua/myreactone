e53dd54cb955f2189735436aad464dc6b9350008
gitjua <juakali422@gmail.com>
2022-06-28 08:44:46 -0600

    Added bloglist

--- src/Home.js
+++ src/Home.js
@@ -1,25 +1,25 @@
 
-import { useState} from 'react';
+import { useState } from "react";
+import BlogList from "./BlogList";
 
 const Home = () => {
-
-  const [name, setName] = useState('Mario');
-  const [age, setAge] = useState(25);
-  const [from, setFrom] = useState('Boston');
-  const handleClick = (e) => {
-   setName('luigi');
-   setAge(30);
-   setFrom('Salt lake City');
-  }
+  const [blogs, setBlogs] = useState([
+    { title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1 },
+    { title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
+    { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }
+  ])
 
   return (
     <div className="home">
-      <h2>Homepage</h2>
-      <p>{name} age is {age} years old and is from {from}</p>
-      <button onClick={handleClick}>Click me</button>
-      
+      {/* {blogs.map(blog => (
+        <div className="blog-preview" key={blog.id} >
+          <h2>{ blog.title }</h2>
+          <p>Written by { blog.author }</p>
+        </div>
+      ))} */}
+      <BlogList blogs={blogs} title="All Blogs" />
     </div>
   );
 }
-  
-  export default Home; 
+ 
+export default Home;
