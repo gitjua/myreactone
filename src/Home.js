74bacc2aa20ecb082106b21a39dd096dafe3f1f3
gitjua <juakali422@gmail.com>
2022-09-06 08:20:52 -0600

    pass delete as prop

--- src/Home.js
+++ src/Home.js
@@ -8,6 +8,10 @@ const Home = () => {
     { title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
     { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }
   ])
+  const handleDelete = (id) => {
+    const newBlogs = blogs.filter(blog => blog.id !== id);
+    setBlogs(newBlogs);
+  }
 
   return (
     <div className="home">
@@ -17,8 +21,8 @@ const Home = () => {
           <p>Written by { blog.author }</p>
         </div>
       ))} */}
-      <BlogList blogs={blogs} title="All Blogs" />
-      <BlogList blogs={blogs.filter(blog => blog.author === 'mario')} title="Mario's Blogs" />
+      <BlogList blogs={blogs} title="All Blogs" handleDelete={handleDelete}  />
+      {/* <BlogList blogs={blogs.filter(blog => blog.author === 'mario')} title="Mario's Blogs" /> */}
     </div>
   );
 }
