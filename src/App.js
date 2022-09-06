f659665e34fd63bf75864ccbf8327eb1952b2ce6
gitjua <juakali422@gmail.com>
2022-09-06 07:27:27 -0600

    removed testClick

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@
 
 import Navbar from './Navbar';
 import Home from './Home';
+
 function App() {
 
   return (
@@ -9,6 +10,7 @@ function App() {
       <Navbar />
       <div className="content">
       <Home />
+
       </div>
     </div>
   );
